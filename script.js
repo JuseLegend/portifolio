bc17db706f2cc3dfec39992348397638f953bb9b
JuseLegend <josemarioschiave@gmail.com>
2025-01-15 15:06:24 -0300

    upload archives

---
+++ script.js
@@ -0,0 +1,4 @@
+// Função para redirecionar ao WhatsApp
+document.getElementById('whatsapp-btn').addEventListener('click', function() {
+    window.location.href = 'https://wa.me/54981138453'; // Substitua 'SEUNUMERO' pelo seu número de WhatsApp
+});
